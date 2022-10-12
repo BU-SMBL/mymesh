15c218a1824b3b53dee3afd03aeb21707bbb496f
TimJosephson <toj@bu.edu>
2022-10-12 16:38:54 -0400

    Update Primitives.py
    Improved runtime and memory efficiency of Grid()

--- Primitives.py
+++ Primitives.py
@@ -5,10 +5,32 @@ Created Sept 2022
 @author: toj
 """
 import numpy as np
+import gc
 from . import *
 
 def Box(bounds,h,meshobj=False):
-    
+    """
+    Box Generate a triangular surface mesh of a rectangular box. 
+
+    Parameters
+    ----------
+    bounds : list
+        Six element list, [xmin,xmax,ymin,ymax,zmin,zmax].
+    h : float
+        Approximate element size.
+    meshobj : bool, optional
+        If true, will return a mesh object, if false, will return 
+        NodeCoords and NodeConn. By default False.
+
+    Returns
+    -------
+    BoxCoords : list
+        Node coordinates of the mesh. Returned if meshobj = False.
+    BoxConn : list
+        Nodal connectivities of the mesh. Returned if meshobj = False.
+    Box : Mesh.mesh
+        Mesh object containing the box mesh. Returned if meshobj = True.
+    """    
     nX = int(np.round((bounds[1]-bounds[0])/h))
     nY = int(np.round((bounds[3]-bounds[2])/h))
     nZ = int(np.round((bounds[5]-bounds[4])/h))
@@ -39,6 +61,70 @@ def Box(bounds,h,meshobj=False):
     if meshobj:
         return Box
     return BoxCoords,BoxConn
+
+def Grid(bounds, h, meshobj=False, exact_h=False):
+    """
+    Grid Generate a rectangular grid mesh.
+
+    Parameters
+    ----------
+    bounds : list
+        Six element list, [xmin,xmax,ymin,ymax,zmin,zmax].
+    h : float
+        Element size.
+    meshobj : bool, optional
+        If true, will return a mesh object, if false, will return 
+        NodeCoords and NodeConn. By default False.
+    exact_h : bool, optional
+        If true, will make a mesh where the element size is exactly
+        equal to what is specified, but the upper bounds may vary slightly.
+        Otherwise, the bounds will be strictly enforced, but element size may deviate
+        from the specified h. This may result in non-cubic elements. By default False.
+
+    Returns
+    -------
+    GridCoords : list
+        Node coordinates of the mesh. Returned if meshobj = False.
+    GridConn : list
+        Nodal connectivities of the mesh. Returned if meshobj = False.
+    Grid : Mesh.mesh
+        Mesh object containing the grid mesh. Returned if meshobj = True.
+    """    
+
+    nX = int(np.round((bounds[1]-bounds[0])/h))
+    nY = int(np.round((bounds[3]-bounds[2])/h))
+    nZ = int(np.round((bounds[5]-bounds[4])/h))
+    if exact_h:
+        xs = np.arange(bounds[0],bounds[1]+h,h)
+        ys = np.arange(bounds[2],bounds[3]+h,h)
+        zs = np.arange(bounds[4],bounds[5]+h,h)
+    else:
+        xs = np.linspace(bounds[0],bounds[1],nX)
+        ys = np.linspace(bounds[2],bounds[3],nY)
+        zs = np.linspace(bounds[4],bounds[5],nZ)
+
+    VoxelCoords = np.hstack([
+        np.repeat(xs,len(ys)*len(zs))[:,None],
+        np.tile(np.repeat(ys,len(xs)),len(zs)).flatten()[:,None],
+        np.tile(np.tile(zs,len(xs)).flatten(),len(ys)).flatten()[:,None]
+    ])
+
+    Ids = np.reshape(np.arange(len(VoxelCoords)),(nX,nY,nZ))
+    
+    VoxelConn = np.zeros(((nX-1)*(nY-1)*(nZ-1),8),dtype=int)
+
+    VoxelConn[:,0] = Ids[:-1,:-1,:-1].flatten()
+    VoxelConn[:,1] = Ids[1:,:-1,:-1].flatten()
+    VoxelConn[:,2] = Ids[1:,1:,:-1].flatten()
+    VoxelConn[:,3] = Ids[:-1,1:,:-1].flatten()
+    VoxelConn[:,4] = Ids[:-1,:-1,1:].flatten()
+    VoxelConn[:,5] = Ids[1:,:-1,1:].flatten()
+    VoxelConn[:,6] = Ids[1:,1:,1:].flatten()
+    VoxelConn[:,7] = Ids[:-1,1:,1:].flatten()
+
+    if meshobj:
+        return mesh(VoxelCoords,VoxelConn)
+    return VoxelCoords, VoxelConn
 
 def Sphere(center,radius,h):
     
