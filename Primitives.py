0aee4179845c9edc2c7f642c09bf865dffbb6df9
TimJosephson <toj@bu.edu>
2022-10-13 14:50:55 -0400

    Update Box to use Grid

--- Primitives.py
+++ Primitives.py
@@ -31,31 +31,9 @@ def Box(bounds,h,meshobj=False):
     Box : Mesh.mesh
         Mesh object containing the box mesh. Returned if meshobj = True.
     """    
-    nX = int(np.round((bounds[1]-bounds[0])/h))
-    nY = int(np.round((bounds[3]-bounds[2])/h))
-    nZ = int(np.round((bounds[5]-bounds[4])/h))
-
-    xs = np.linspace(bounds[0],bounds[1],nX)
-    ys = np.linspace(bounds[2],bounds[3],nY)
-    zs = np.linspace(bounds[4],bounds[5],nZ)
-    X, Y, Z = np.meshgrid(xs,ys,zs,indexing='ij')
-    Xshape = X.shape
-    x = X.flatten()
-    y = Y.flatten()
-    z = Z.flatten()
-    ids = np.arange(len(x))
-    Ids = np.reshape(ids,Xshape)
-    VoxelCoords = np.vstack([x,y,z]).transpose()
-    VoxelConn = [[] for i in range(nX-1) for j in range(nY-1) for k in range(nZ-1)] 
-    l = 0
-    for i in range(nX-1):
-        for j in range(nY-1):
-            for k in range(nZ-1):
-                idxs = [Ids[i,j,k],Ids[i+1,j,k],Ids[i+1,j+1,k],Ids[i,j+1,k],Ids[i,j,k+1],Ids[i+1,j,k+1],Ids[i+1,j+1,k+1],Ids[i,j+1,k+1]]
-                VoxelConn[l] = idxs
-                l += 1
-    BoxCoords,VoxelConn,_ = converter.removeNodes(VoxelCoords,VoxelConn)
-    BoxConn = converter.quad2tri(converter.solid2surface(BoxCoords,VoxelConn))
+    GridCoords, GridConn = Grid(bounds,h,exact_h=False)
+    BoxConn = converter.quad2tri(converter.solid2surface(GridCoords,GridConn))
+    BoxCoords,BoxConn = converter.removeNodes(GridCoords,BoxConn)
     Box = mesh(BoxCoords,BoxConn)
     Box.cleanup()
     if meshobj:
