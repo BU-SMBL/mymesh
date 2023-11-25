5cf27b608b6386886608d2861c82a4fc115f2d62
TimJosephson <toj@bu.edu>
2023-11-25 17:24:08 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,19 +4,4 @@ Created on Thu Mar 24 13:51:50 2022
 
 @author: toj
 """
-from . import converter
-from . import booleans
-from . import curvature
-from . import delaunay
-from . import implicit
-from . import MarchingCubes
 from .mesh import mesh
-from . import utils
-from . import octree
-from . import parseAnsys
-from . import parseINP
-from . import parseSTL
-from . import quality
-from . import rays
-from . import TetGen
-from . import primitives
