a98090a16c5554431711427ff90aad2474c9d392
TimJosephson <toj@bu.edu>
2024-07-30 11:36:07 -0400

    Add numba utilities

--- src/mymesh/__init__.py
+++ src/mymesh/__init__.py
@@ -38,5 +38,45 @@ Submodules
     visualize
 
 """
+from functools import wraps
+
+try: 
+    from numba import njit
+    _MYMESH_USE_NUMBA = True
+except ImportError:
+    njit = None
+    _MYMESH_USE_NUMBA = False
+
+def use_numba(enabled=True):
+    global _MYMESH_USE_NUMBA
+    if njit is None:
+        warnings.warn('numba is not available for import. Install with `conda install numba` or `pip install numba`.')
+    _MYMESH_USE_NUMBA = enabled and (njit is not None)
+
+def check_numba():
+    global _MYMESH_USE_NUMBA
+    if _MYMESH_USE_NUMBA and (njit is not None):
+        check = True
+    else:
+        check = False
+    return check
+
+def try_njit(func=None, *njit_args, **njit_kwargs):
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if _MYMESH_USE_NUMBA:
+                jit_func = njit(*njit_args, **njit_kwargs)(func)
+                return jit_func(*args, **kwargs)
+            else:
+                return func(*args, **kwargs)
+        return wrapper
+    
+    return decorator(func) if func else decorator
+
 from .mesh import mesh
 from . import booleans, contour, curvature, delaunay, image, implicit, improvement, octree, primitives, quality, utils, visualize
+__all__ = ["use_numba", "try_njit", "mesh", "booleans", "contour", "curvature",
+"delaunay", "image", "implicit", "improvement", "octree", "primitives", 
+"quality", "utils", "visualize", "_MYMESH_USE_NUMBA"]
